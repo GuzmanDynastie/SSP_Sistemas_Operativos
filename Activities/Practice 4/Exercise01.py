cd30a0bc4eef610adba925097901dc328fdb122c
Guzman <guzmanjrpro@gmail.com>
2024-02-16 21:40:22 -0600

    Added new code lines for create multiple bar graph for ('número de personas en cada clase')
    Unfinished exercise

--- Activities/Practice 4/Exercise01.py
+++ Activities/Practice 4/Exercise01.py
@@ -22,6 +22,7 @@ titanic = file_titanic
 
 
 # Diagrama de sectores con los fallecidos y supervivientes.
+'''
 deceased = int(titanic[titanic.Survived == 0].shape[0])
 survivor = int(titanic[titanic.Survived == 1].shape[0])
 
@@ -32,6 +33,7 @@ survived = ['Fallecidos', 'Sobrevivientes']
 values = np.array([deceased, survivor])
 ax.pie(values, labels=survived, autopct='%1.2f%%')
 plt.show()
+'''
 
 # Histograma con las edades.
 '''
@@ -50,7 +52,7 @@ unique_numbers, counts = np.unique(array_sorted, return_counts=True)
 for i, (number, count) in enumerate(zip(unique_numbers, counts)):
     print(f"Número {number} aparece {count} veces.")
 '''
-
+'''
 titanic['Age'] = pd.to_numeric(titanic['Age'], errors='coerce') # Convierte 'Age' a tipo numérico
 
 not_ages = titanic['Age'].isna().sum() # Calcula el número de edades sin registro
@@ -87,5 +89,35 @@ ax.legend(bars, legend_labels)
 # Añadir etiquetas con los valores en las barras
 for i, v in enumerate(counts):
     ax.text(i, v + 5, str(v), ha='center', va='bottom')
+
+plt.show()
+'''
+
+
+# Diagrama de barras con el número de personas en cada clase.
+
+class_titanic = np.array(titanic['Pclass']) # Obtengo el arreglo de todas las clases
+array_sorted = np.sort(class_titanic) # Ordeno todas las clases
+
+unique_values, counts = np.unique(array_sorted, return_counts=True) # Retorna el conteo del valor y del cuantas veces se repite
+
+for value, count in zip(unique_values, counts): # Esto es opcional
+    print(f'El número {value} se repite {count} veces') # Imprime el numero y la suma de las repeticiones del mismo
+
+fig, ax = plt.subplots() # Crea una nueva figura y un conjunto de ejes
+bars = ax.bar(range(len(unique_values)), counts, color=sns.color_palette('husl', len(unique_values))) # Se crean los graficos en el eje ax (sirve para reducir codigo)
+
+ax.set_xticks(range(len(unique_values))) # Se proporciona la secuencia de posiciones generada con range(len(unique_values))
+ax.set_xticklabels(unique_values) # Se proporciona el array unique_values como etiquetas.
+
+ax.set_xlabel('Clases')
+ax.set_ylabel('Cantidades')
+ax.set_title('Número de personas en cada clase')
+
+labels = (f'Clase {i + 1}' for i in range(len(unique_values))) # Esto crea una expresión generadora que produce etiquetas del tipo "Clase 1", "Clase 2", ..., "Clase n", donde n es la longitud de unique_values. 
+ax.legend(bars, labels) #  Esto agrega una leyenda al gráfico utilizando las barras (bars) y las etiquetas proporcionadas.
+
+for i, v in zip(range(len(unique_values)), counts): # Asigna los datos correspondientes a cada barra
+    ax.text(i, v, str(v), ha='center', va='bottom')
 
 plt.show()
