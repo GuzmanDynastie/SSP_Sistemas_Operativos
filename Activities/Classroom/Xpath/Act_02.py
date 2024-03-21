92b5e377dca138aa0ef832cdd354fc5395852085
Guzman <guzmanjrpro@gmail.com>
2024-03-20 22:50:24 -0600

    New requirements have been added to the exercices

---
+++ Activities/Classroom/Xpath/Act_02.py
@@ -0,0 +1,20 @@
+from lxml import etree
+
+tree = etree.parse("Activities/Classroom/Xpath/Act_02.xml")
+
+# • Mostrar el nombre sin etiquetas de los módulos que se imparten en el instituto.
+
+
+# • Mostrar el nombre de los módulos que se imparten en ciclo formativo ASIR.
+
+
+# • Mostrar el nombre de los módulos que se imparten en segundo curso de cualquier ciclo formativo.
+
+
+# • Mostrar el nombre de los módulos que tengan menos de 5 horas semanales.
+
+
+# • Mostrar el nombre de los módulos que se imparten en el primer curso del ciclo formativo ASIR.
+
+
+# • Mostrar las horas semanales sin etiquetas de los módulos que se imparten en más de 3 horas semanales 
---
+++ Activities/Classroom/Xpath/Act_03.py
@@ -0,0 +1,17 @@
+from lxml import etree
+
+tree = etree.parse("Activities/Classroom/Xpath/Act_03.xml")
+
+# • Mostrar los nombres de los módulos del ciclo de "Sistemas Microinformáticos y Redes".
+
+
+# • Mostrar los nombres de los ciclos formativos que incluyen el módulo "Lenguajes de marcas y sistemas de gestión de información".
+
+
+# • Mostrar los nombres de los módulos de ciclos de Grado Superior.
+
+
+# • Mostrar los nombres de los módulos de los ciclos formativos cuyo título se aprobó en 2008.
+
+
+# • Mostrar los grados de los ciclos formativos con módulos de primer curso. 
