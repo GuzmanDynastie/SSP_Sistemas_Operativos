377026f72b3e387a400981013d1df32f0910b2af
Guzman <guzmanjrpro@gmail.com>
2024-04-08 22:51:41 -0600

    example of selenium

---
+++ Activities/Classroom/selenium/act_prueba.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+
+# PATH = "C:/Users/guzma/OneDrive/Escritorio/chromedriver.exe"
+PATH = ".//Activities//Classroom//selenium//chromedriver.exe"
+driver = webdriver.Chrome(PATH)
+
+driver.get("https://google.com.mx")
+
+# Buscar el cuadro de búsqueda de Google por su nombre
+search_box = driver.find_element_by_name('q')
+
+# Simular la escritura en el cuadro de búsqueda
+search_box.send_keys("peso pluma")
+
+# Enviar el formulario (simulando presionar la tecla Enter)
+search_box.submit()
+
+# driver.quit()
