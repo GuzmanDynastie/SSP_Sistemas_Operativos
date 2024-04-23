89d7ed9fa30f065ee5c688693dfe6e6ca380618f
Guzman <guzmanjrpro@gmail.com>
2024-04-23 07:41:56 -0600

    Code has been modified for searching for anything in Google

--- Activities/Classroom/selenium/act_prueba.py
+++ Activities/Classroom/selenium/act_prueba.py
@@ -1,7 +1,8 @@
 from selenium import webdriver
+import time
 
 # PATH = "C:/Users/guzma/OneDrive/Escritorio/chromedriver.exe"
-PATH = ".//Activities//Classroom//selenium//chromedriver.exe"
+PATH = ".\Activities\Classroom\selenium\chromedriver.exe"
 driver = webdriver.Chrome(PATH)
 
 driver.get("https://google.com.mx")
@@ -15,4 +16,6 @@ search_box.send_keys("peso pluma")
 # Enviar el formulario (simulando presionar la tecla Enter)
 search_box.submit()
 
-# driver.quit()
+
+
+driver.quit()
